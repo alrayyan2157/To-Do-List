0f08d9c794c23e040cce77a0aec4908b59b85844
AlRayyan Arshad <alrayyan2157@icloud.com>
2026-06-15 19:53:57 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import './styles.css';
+
+const root = document.getElementById('root');
+const heading = document.createElement('h1');
+heading.textContent = 'Hello, Webpack!';
+root.appendChild(heading);
