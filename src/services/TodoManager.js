cc84109a2c8f7ad3fe9c58b69bafc3ff923808d1
alrayyan2157 <alrayyan2157@icloud.com>
2026-06-15 21:03:34 +0530

    Creating project handeling file

---
+++ src/services/TodoManager.js
@@ -0,0 +1,36 @@
+import { Project } from '../models/Project.js';
+
+export class TodoManager {
+  constructor() {
+    this.projects = [];
+    this.ensureDefaultProject();
+  }
+
+  ensureDefaultProject() {
+    if (this.projects.length === 0) {
+      this.createProject('Inbox');
+    }
+  }
+
+  createProject(name) {
+    if (this.projects.some(p => p.name.toLowerCase() === name.toLowerCase())) {
+      throw new Error('Project name already exists.');
+    }
+    const newProject = new Project(name);
+    this.projects.push(newProject);
+    return newProject;
+  }
+
+  deleteProject(projectId) {
+    this.projects = this.projects.filter(p => p.id !== projectId);
+    this.ensureDefaultProject();
+  }
+
+  getProjects() {
+    return this.projects;
+  }
+
+  getProjectById(projectId) {
+    return this.projects.find(p => p.id === projectId);
+  }
+}
